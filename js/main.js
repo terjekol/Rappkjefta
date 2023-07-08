e4fbd9fffb41ac158e7db80877317e71f9570994
Kristoffer Kolderup <69051007+kikiokol@users.noreply.github.com>
2023-07-08 18:41:32 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,21 +2,121 @@ let model = {
     letters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ',
     currentLetter: null,
     categories: `spill
-    navn
-    guttenavn
-    jentenavn
-    geografisk stedsnavn
-    noe jeg kan se
-    noe jeg ikke kan se
-    land
-    by
-    bilselskap
-    transportmiddel
-    matrett
-    noe man kan spise
-    et verb
-    en bokstav i alfabetet
-    et adjektiv`.split("\n"),
+    Navn
+    Guttenavn
+    Jentenavn
+    Geografisk stedsnavn
+    Noe jeg kan se
+    Noe jeg ikke kan se
+    Land
+    By
+    Bilselskap
+    Transportmiddel
+    Matrett
+    Noe man kan spise
+    Et verb
+    En bokstav i alfabetet
+    Et adjektiv
+    Frukt
+    Grønnsak
+    Musiker
+    Film
+    Dyreart
+    Idrettsutøver
+    Bygning
+    Filmkarakter
+    Skuespiller
+    Kjendis
+    Superhelt
+    Bilmakro
+    Sport
+    Drikke
+    Musikkgenre
+    Instrument
+    Yrke
+    TV-serie
+    Landemerke
+    Vitenskapelig begrep
+    Musikkband
+    Oppfinnelse
+    Høytidsdag
+    Måned
+    Møbel
+    Dyr i Afrika
+    Matvaremerke
+    Nettsted
+    Videospill
+    Motevaremerke
+    Vitenskapelig fag
+    Kjæledyr
+    Matrett fra Asia
+    Musikkalbum
+    Idrettsspill
+    Romanforfatter
+    Tegneseriefigur
+    Kjærlighetsfilm
+    Politisk leder
+    Teaterstykke
+    Skoledisiplin
+    Matvare i boks
+    TV-kokk
+    Verdensrekord
+    Musikkfestival
+    Friluftsaktivitet
+    Naturfenomen
+    Bokanmelder
+    Vokalgruppe
+    Fotograf
+    Bryggeri
+    Kjæledyrsside
+    TV-show
+    Romanfigur
+    Matlagingsteknikk
+    Nobelprisvinner
+    Automerket
+    Legendarisk idrettslag
+    Romvesen
+    Sjakkstykke
+    Kjent maleri
+    Reisemål
+    Vinprodusent
+    Oppdagelsesreisende
+    Klesdesigner
+    Bokforfatter
+    Fotballturnering
+    Politisk parti
+    Skogdyr
+    Stjernetegn
+    Vintype
+    Filmregissør
+    Dataprogram
+    Vitenskapelig oppdagelse
+    Musikkpris
+    Skummelt sted
+    Barnebokforfatter
+    Sangtekst
+    Historisk hendelse
+    Dansesjanger
+    Eksotisk frukt
+    TV-realityshow
+    Verdensleder
+    Kjøkkenredskap
+    Fotballspiller
+    Verdenshav
+    Fototeknikk
+    Skiskisted
+    Bilmodell
+    Artist i Grammy Awards
+    Bokstav i navnet
+    Astrologisk tegn
+    Prisvinnende film
+    Bysymbol
+    Mode designer
+    Populært nettsted
+    Gresk gud
+    Oppdagelsesreisendes reisemål
+    Hjemmedyrket frukt
+    Slangord`.split("\n"),
     currentCategory: null
 };
 
